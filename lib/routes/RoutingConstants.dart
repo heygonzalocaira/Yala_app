62e3772f137136cac5066dd2519afa3bb4fda37a
GonzaloCaira <gcairaaguilar@gmail.com>
2021-06-21 17:39:49 -0500

    Adding route to app

---
+++ lib/routes/RoutingConstants.dart
@@ -0,0 +1,21 @@
+class RouteNames {
+  static const String TaskRoute = "TaskNavigator";
+  static const String HomeRoute = "HomeNavigator";
+  static const String ProfileRoute = "ProfileNavigator";
+  // Navigator to screens | button navigatorapp
+  //
+  static const String ClassicGame = "ClassicGameScreen";
+  static const String BRoyalGame = "BRoyalGameScreen";
+  //
+  static const String LoginRoute = "LoginScreen";
+
+  //
+  static const ListRoutes = [
+    TaskRoute,
+    HomeRoute,
+    ProfileRoute,
+    ClassicGame,
+    BRoyalGame,
+    LoginRoute
+  ];
+}
